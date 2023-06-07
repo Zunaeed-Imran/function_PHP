3888dbfc3427a45566746a8c1793c12b8b65c1fe
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-07 13:10:05 +0600

    string functions in php.
    function to lowercase, to upper, trim, reverse, replace, pad, etc.

---
+++ stringFunction.php
@@ -0,0 +1,14 @@
+<?php
+  $username = "Imran";
+  $phone = "125-954-258";
+
+// $username = strtolower($username);
+// $username = strtoupper($username);
+// $username = trim($username); it will trim the spaces
+// $username = str_pad($username, 20, "0");
+$username = strrev($username);
+  //  $phone = str_replace("-", " oh ", $phone);
+
+    echo $username;
+    // echo $phone;
+?>
