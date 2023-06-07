6b2f4c15b603e4281320c2e29caa30193a0e088f
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-07 12:19:34 +0600

    basic function in php

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+  function happy_birthday(){
+    echo "Happy Birthday to you </br>";
+    echo "Happy Birthday to you </br>";
+    echo "Happy Birthday dear x </br>";
+    echo "Happy Birthday to you </br>";
+  }
+
+  happy_birthday();
+?>
