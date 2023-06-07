b9653b0ac6fd18fd3e3bd568946e518fd1a84e5f
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-07 12:31:09 +0600

    Function argument &parameter
    down side call argument. 
    and up with function call parameter.

---
+++ function2.php
@@ -0,0 +1,12 @@
+<?php
+  function happy_birthday($name, $age){
+    echo "Happy Birthday to you </br>";
+    echo "Happy Birthday to you </br>";
+    echo "Happy Birthday dear {$name} {$age} </br>";
+    echo "Happy Birthday to you </br></br>";
+  }
+
+  happy_birthday("spongebob", 30);
+  happy_birthday("imran", 26);
+  happy_birthday("spartacus", 30);
+?>
